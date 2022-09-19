9a86a0186cc4f833ee2b9f6cefd15b61bbbcbbff
ACEGAZ <garylowles@gmail.com>
2022-09-19 11:57:05 +0000

    add all forms for commissions

---
+++ website/forms.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm
+from .models import RegularCommission
+from .models import ReferenceSheetCommission
+from .models import CustomCommissions
+from .models import AddArt
+
+
+class RegularCommissionForm(ModelForm):
+    """regular form for commission_view """
+    class Meta:
+        """creates all fields for RegularCommissionForm """
+        model = RegularCommission
+        fields = '__all__'
+
+
+class ReferenceSheetForm(ModelForm):
+    """reference sheet form for commission_view """
+    class Meta:
+        """creates all fields for ReferenceSheetForm """
+        model = ReferenceSheetCommission
+        fields = '__all__'
+
+
+class CustomForm(ModelForm):
+    """custom form for commission_view """
+    class Meta:
+        """creates all fields for CustomForm """
+        model = CustomCommissions
+        fields = '__all__'
