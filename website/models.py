b5054f3152dec40df83523cdbf0066a8e3a3e635
ACEGAZ <garylowles@gmail.com>
2022-09-19 11:50:06 +0000

    add addart model in models.py

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), 1, "Published")
+
+
+class AddArt(models.Model):
+    """add artwork to website """
+    title = models.CharField(max_length=200, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.title
