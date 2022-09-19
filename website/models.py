d438833277368ace9e4f323f0a22b210f6627f59
ACEGAZ <garylowles@gmail.com>
2022-09-19 11:51:05 +0000

    add regularcommission model in models.py

--- website/models.py
+++ website/models.py
@@ -14,3 +14,18 @@ class AddArt(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class RegularCommission(models.Model):
+    """contact form for users to request regular commisions"""
+    character_reference = models.CharField(max_length=100)
+    character_owner = models.CharField(max_length=100)
+    commission_type = models.CharField(max_length=100)
+    type_option = models.CharField(max_length=100,)
+    character_personality = models.CharField(max_length=100)
+    pose = models.CharField(max_length=100)
+    other_info = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.email
